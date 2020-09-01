dbd9bbae9d6ab6822f253fb1a9283844b5725697
Régis NUTTIN <regisnut@hotmail.com>
2020-09-01 10:33:35 +0200

    project menu finalized

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,38 @@
-import { Link } from "gatsby"
-import React from "react"
+import React, { useEffect } from 'react';
+import { Link } from 'gatsby';
 
-const Header = () => {
-  return (
-    <header>
-      <div className="container fluid">
-        <div className="header-inner">
-          <Link activeClassName="active" to="/">
-            Pocket.
-          </Link>
-          <div className="hamburger-menu">
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-      </div>
-    </header>
-  )
-}
+import { useLocation } from '@reach/router';
 
-export default Header
+const Header = ({ setMenuState, menuState, setCursorHovered }) => {
+	//tips pour avoir le menu fermé à chq chgt d page
+	const location = useLocation();
+	useEffect(
+		() => {
+			setMenuState(false);
+		},
+		[ location ]
+	);
+
+	return (
+		<header>
+			<div className="container fluid">
+				<div className="header-inner">
+					<Link activeClassName="active" to="/">
+						Pocket.
+					</Link>
+					<div
+						onClick={() => setMenuState(true)}
+						className="hamburger-menu"
+						onMouseEnter={() => setCursorHovered(true)}
+						onMouseLeave={() => setCursorHovered(false)}
+					>
+						<span />
+						<span />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,59 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+import useMousePosition from '../hooks/useMousePosition';
 
 //Components
-import Header from "./header"
-
+import Header from './header';
+import Menu from './menu';
 //Styles
-import "../styles/App.scss"
+import '../styles/App.scss';
+import { motion } from 'framer-motion';
 
 const Layout = ({ children }) => {
-  const siteData = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+	const siteData = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`);
 
-  return (
-    <div className="app">
-      <Header siteTitle={siteData.site.siteMetadata.title} />
-      <div>
-        <main>{children}</main>
-      </div>
-    </div>
-  )
-}
+	const [ menuState, setMenuState ] = useState(false);
+	const [ cursorHovered, setCursorHovered ] = useState(false);
+
+	const { x, y } = useMousePosition();
+
+	return (
+		<div className="app">
+			<motion.div
+				animate={{
+					x: x - 20,
+					y: y - 20,
+					scale: cursorHovered ? 1.2 : 1,
+					opacity: cursorHovered ? 0.8 : 0
+				}}
+				transition={{ ease: 'linear', duration: 0.2 }}
+				className="cursor"
+			/>
+			<Header
+				setCursorHovered={setCursorHovered}
+				menuState={menuState}
+				setMenuState={setMenuState}
+				siteTitle={siteData.site.siteMetadata.title}
+			/>
+			<Menu setCursorHovered={setCursorHovered} x={x} y={y} menuState={menuState} setMenuState={setMenuState} />
+			<div>
+				<main>{children}</main>
+			</div>
+		</div>
+	);
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+	children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
